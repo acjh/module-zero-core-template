001112a03f9b8b9df2ba26bca0df5a594dc70df0
Aaron Chong <aaronjhchong@hotmail.com>
2018-08-19 10:21:26 +0800

    Add demo for SettingDefinitionGroup

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Configuration/AppSettingProvider.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Core/Configuration/AppSettingProvider.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections.Generic;
 using Abp.Configuration;
+using Abp.Localization;
 
 namespace AbpCompanyName.AbpProjectName.Configuration
 {
     public class AppSettingProvider : SettingProvider
     {
+        public static List<SettingDefinitionGroup> Groups = new List<SettingDefinitionGroup>();
+
         public override IEnumerable<SettingDefinition> GetSettingDefinitions(SettingDefinitionProviderContext context)
         {
+            var group1 = new SettingDefinitionGroup("group1_name", new LocalizableString("group1_displayName", "sourceName"));
+            var group2 = new SettingDefinitionGroup("group2_name", new LocalizableString("group2_displayName", "sourceName"));
+
+            Groups.Add(group1);
+            Groups.Add(group2);
+            group1.AddChild(group2);
+
             return new[]
             {
+                new SettingDefinition("setting1_name", "setting1_defaultValue", group: group1),
+                new SettingDefinition("setting2_name", "setting2_defaultValue", group: group2),
                 new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, isVisibleToClients: true)
             };
         }
--- aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Host/Controllers/HomeController.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Host/Controllers/HomeController.cs
@@ -5,21 +5,81 @@ using Abp.Extensions;
 using Abp.Notifications;
 using Abp.Timing;
 using AbpCompanyName.AbpProjectName.Controllers;
+using AbpCompanyName.AbpProjectName.Configuration;
+using System.Linq;
+using Abp.Configuration;
+using System.Collections.Generic;
 
 namespace AbpCompanyName.AbpProjectName.Web.Host.Controllers
 {
     public class HomeController : AbpProjectNameControllerBase
     {
         private readonly INotificationPublisher _notificationPublisher;
+        private readonly ISettingDefinitionManager _settingDefinitionManager;
 
-        public HomeController(INotificationPublisher notificationPublisher)
+        public HomeController(
+            INotificationPublisher notificationPublisher,
+            ISettingDefinitionManager settingDefinitionManager)
         {
             _notificationPublisher = notificationPublisher;
+            _settingDefinitionManager = settingDefinitionManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var rootSettingDefinitionGroupDto = await GetRootSettingDefinitionGroupDto();
+
             return Redirect("/swagger");
+        }
+
+        public async Task<SettingDefinitionGroupDto> GetRootSettingDefinitionGroupDto()
+        {
+            var groups = AppSettingProvider.Groups;
+            var rootGroups = groups.Where(g => g.Parent == null).ToList();
+            var settingDefinitions = _settingDefinitionManager.GetAllSettingDefinitions();
+            var groupedSettingDefinitions = settingDefinitions.Where(sd => sd.Group != null).ToList();
+            var settings = await SettingManager.GetAllSettingValuesAsync();
+
+            return new SettingDefinitionGroupDto
+            {
+                Children = GetChildrenGroupDtos(rootGroups, groupedSettingDefinitions, settings),
+                GroupName = null,
+                Settings = settingDefinitions
+                    .Where(sd => sd.Group == null)
+                    .Select(sd => settings.Where(s => s.Name == sd.Name).FirstOrDefault())
+                    .Where(s => s != null)
+                    .ToList()
+            };
+        }
+
+        public class SettingDefinitionGroupDto
+        {
+            public List<SettingDefinitionGroupDto> Children { get; set; }
+            
+            public string GroupName { get; set; }
+
+            public List<ISettingValue> Settings { get; set; }
+        }
+
+        private List<SettingDefinitionGroupDto> GetChildrenGroupDtos(IReadOnlyList<SettingDefinitionGroup> groups, IReadOnlyList<SettingDefinition> settingDefinitions, IReadOnlyList<ISettingValue> settings)
+        {
+            return groups
+                .Select(group => new SettingDefinitionGroupDto
+                {
+                    Children = GetChildrenGroupDtos(group.Children, settingDefinitions, settings),
+                    GroupName = group.Name,
+                    Settings = GetGroupSettings(group, settingDefinitions, settings)
+                })
+                .ToList();
+        }
+
+        private List<ISettingValue> GetGroupSettings(SettingDefinitionGroup group, IReadOnlyList<SettingDefinition> settingDefinitions, IReadOnlyList<ISettingValue> settings)
+        {
+            return settingDefinitions
+                .Where(sd => sd.Group.Name == group.Name)
+                .Select(sd => settings.Where(s => s.Name == sd.Name).FirstOrDefault())
+                .Where(s => s != null)
+                .ToList();
         }
 
         /// <summary>
