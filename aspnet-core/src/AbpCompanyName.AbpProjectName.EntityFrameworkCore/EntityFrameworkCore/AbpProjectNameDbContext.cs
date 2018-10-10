3baa69fe5ff2c4b3c1c59029b3ce0591a7c28e76
Aaron Chong <aaronjhchong@hotmail.com>
2018-10-11 00:58:18 +0800

    Handle implicit delete for soft delete

--- aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/EntityFrameworkCore/AbpProjectNameDbContext.cs
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.EntityFrameworkCore/EntityFrameworkCore/AbpProjectNameDbContext.cs
@@ -3,16 +3,83 @@ using Abp.Zero.EntityFrameworkCore;
 using AbpCompanyName.AbpProjectName.Authorization.Roles;
 using AbpCompanyName.AbpProjectName.Authorization.Users;
 using AbpCompanyName.AbpProjectName.MultiTenancy;
+using System.Linq;
+using Abp.Collections.Extensions;
+using Abp.Events.Bus.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System.Collections.Generic;
 
 namespace AbpCompanyName.AbpProjectName.EntityFrameworkCore
 {
+    public class Proposal : Entity
+    {
+        public ICollection<TravelOfferPriceProposalElement> TravelOfferPriceProposalElements { get; set; }
+    }
+
+    public class TravelOfferPriceProposalElement : FullAuditedEntity
+    {
+    }
+
     public class AbpProjectNameDbContext : AbpZeroDbContext<Tenant, Role, User, AbpProjectNameDbContext>
     {
         /* Define a DbSet for each entity of the application */
         
+        public DbSet<Proposal> Proposals { get; set; }
+
+        public DbSet<TravelOfferPriceProposalElement> TravelOfferPriceProposalElements { get; set; }
+
+
         public AbpProjectNameDbContext(DbContextOptions<AbpProjectNameDbContext> options)
             : base(options)
         {
+        }
+
+        protected int? CurrentCompanyId => GetCurrentCompanyIdOrNull();
+
+        protected virtual int? GetCurrentCompanyIdOrNull()
+        {
+            if (CurrentUnitOfWorkProvider != null &&
+                CurrentUnitOfWorkProvider.Current != null)
+            {
+                return CurrentUnitOfWorkProvider.Current
+                    .Filters.FirstOrDefault(f => f.FilterName == "CompanyFilter")?
+                    .FilterParameters.GetOrDefault("CompanyId") as int?;
+            }
+
+            return null;
+        }
+
+        protected override void ApplyAbpConcepts(EntityEntry entry, long? userId, EntityChangeReport changeReport)
+        {
+            HandleImplicitDeleteForSoftDelete(entry);
+            base.ApplyAbpConcepts(entry, userId, changeReport);
+        }
+
+        protected virtual void HandleImplicitDeleteForSoftDelete(EntityEntry entry)
+        {
+            if (entry.State == EntityState.Modified && entry.Entity is ISoftDelete)
+            {
+                var foreignKeys = entry.Metadata.GetForeignKeys();
+
+                foreach (var foreignKey in foreignKeys)
+                {
+                    foreach (var property in foreignKey.Properties)
+                    {
+                        var propertyEntry = entry.Property(property.Name);
+                        if (propertyEntry.OriginalValue != null && propertyEntry.CurrentValue == null)
+                        {
+                            entry.State = EntityState.Deleted;
+                        }
+                    }
+                }
+            }
+        }
+
+        protected override void ApplyAbpConceptsForDeletedEntity(EntityEntry entry, long? userId, EntityChangeReport changeReport)
+        {
+            base.ApplyAbpConceptsForDeletedEntity(entry, userId, changeReport);
         }
     }
 }
---
+++ aspnet-core/test/AbpCompanyName.AbpProjectName.Tests/Proposals/ProposalRepository_Tests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using AbpCompanyName.AbpProjectName.EntityFrameworkCore;
+
+namespace AbpCompanyName.AbpProjectName.Tests.Proposals
+{
+    public class ProposalRepository_Tests : AbpProjectNameTestBase
+    {
+        private readonly IRepository<Proposal> _proposalRepository;
+        private readonly IRepository<TravelOfferPriceProposalElement> _proposalElementRepository;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public ProposalRepository_Tests()
+        {
+            _proposalRepository = Resolve<IRepository<Proposal>>();
+            _proposalElementRepository = Resolve<IRepository<TravelOfferPriceProposalElement>>();
+            _unitOfWorkManager = Resolve<IUnitOfWorkManager>();
+        }
+
+        [Fact]
+        public async Task FilterProposals_Test()
+        {
+            await _proposalRepository.InsertAsync(new Proposal
+            {
+                TravelOfferPriceProposalElements = new List<TravelOfferPriceProposalElement>
+                {
+                    new TravelOfferPriceProposalElement()
+                }
+            });
+
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                var proposal = await _proposalRepository.GetAll()
+                    .Include(b => b.TravelOfferPriceProposalElements)
+                    .FirstAsync(b => b.Id == 0);
+
+                proposal.TravelOfferPriceProposalElements = new List<TravelOfferPriceProposalElement>();
+
+                await uow.CompleteAsync();
+            }
+
+            using (var uow = _unitOfWorkManager.Begin())
+            {
+                _proposalElementRepository.Count().ShouldBe(0);
+
+                using (_unitOfWorkManager.Current.DisableFilter(AbpDataFilters.SoftDelete))
+                {
+                    _proposalElementRepository.Count().ShouldBe(1);
+                }
+
+                await uow.CompleteAsync();
+            }
+        }
+    }
+}
